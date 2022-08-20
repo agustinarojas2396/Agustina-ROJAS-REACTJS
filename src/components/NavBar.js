1676cb67a077b41d9b80716e25df0a8cf2c18768
Agustina <agussrojas.00@gmail.com>
2022-08-20 01:08:51 -0300

    menu casi listo

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return ( 
+
+        <div className="all">
+        <section className="nav-container">
+            <h1 className="title">E-COMMERCE</h1>
+            <ul className="menu">
+                <li className="items">Menu</li>
+                <li className="items">About Us</li>
+                <li className="items">Contact</li>
+            </ul>
+        </section>
+        </div>
+    )
+
+
+
+
+}
+
+export default NavBar
