b5604cadedd79b45969ee765a6e052d331c9a0f0
Agustina <agussrojas.00@gmail.com>
2022-08-22 20:26:10 -0300

    menu bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import NavBar from './components/NavBar.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,27 @@
 import './NavBar.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+
 
 const NavBar = () => {
     return ( 
 
-        <div className="all">
-        <section className="nav-container">
-            <h1 className="title">E-COMMERCE</h1>
-            <ul className="menu">
-                <li className="items">Menu</li>
-                <li className="items">About Us</li>
-                <li className="items">Contact</li>
-            </ul>
-        </section>
-        </div>
-    )
+        <Navbar bg="dark" variant="dark" className="menu">
+        <Container className="menu1" >
+            <Navbar.Brand href="#home">E-commerce</Navbar.Brand>
+            <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#About">About Us</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+        </Container>
+        </Navbar>
 
-
-
-
+);
 }
 
 export default NavBar
